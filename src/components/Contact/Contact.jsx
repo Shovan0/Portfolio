ee332eaebceb63d9160616db78232768d3e7c387
DipanwitaDas <dipanwitadas2442004@gmail.com>
2025-05-20 01:29:01 +0530

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from "./Contact.module.css";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+        <div className={styles.text}>
+            <h2>Contact</h2>
+            <p>Feel free to reach out!</p>
+        </div>
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
+                <a href="mailto:dipanwitadas2442004@gmail.com">dipanwitadas2442004@gmail.com</a>
+            </li>
+
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn Icon" />
+                <a href="https://www.linkedin.com/in/dipanwita-das-115621280/">https://www.linkedin.com/in/dipanwita-das-115621280/</a>
+            </li>
+
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/githubIcon.png")} alt="Github Icon" />
+                <a href="https://github.com/DipanwitaDas">https://github.com/DipanwitaDas</a>
+            </li>
+        </ul>
+    </footer>
+  )
+}
