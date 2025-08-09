8a6d157d4ff3f742ffea7fe5d204e9dfcad99b71
Shovan0 <shovannath4039@gmail.com>
2025-08-09 18:12:33 +0530

    9.8.25  18.12

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
             <h1 className={styles.title}>Hi, I'm Shovan</h1>
             <p className={styles.description}>I'm a full-stack developer with freashers using React and NodeJS. Reach out if you'd like to learn more!</p>
             <a
-              href="https://mail.google.com/mail/?view=cm&fs=1&to=shovannath2001@gmail.com&su=Portfolio%20Contact&body=Hi%20Dipanwita%2C%0AI%20saw%20your%20portfolio%20and%20wanted%20to%20connect."
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=shovannath2001@gmail.com&su=Portfolio%20Contact&body=Hi%20Shovan%2C%0AI%20saw%20your%20portfolio%20and%20wanted%20to%20connect."
               target="_blank"
               rel="noopener noreferrer"
               className={styles.contactBtn}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './Navbar.module.css';
-// import { getImageUrl } from '../../utils';
 import close from "../../../public/assets/nav/closeIcon.png"
 import menu from "../../../public/assets/nav/menuIcon.png"
 
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      // Close the menu if the click is outside the nav element
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    };
+
+    // Only add the listener if the menu is open
+    if (menuOpen) {
+      window.addEventListener('click', handleOutsideClick);
+    }
+
+    // Clean up the event listener when the component unmounts or menuOpen changes
+    return () => {
+      window.removeEventListener('click', handleOutsideClick);
+    };
+  }, [menuOpen]); // Re-run effect when menuOpen state changes
 
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles.navbar} ref={navRef}>
       <a className={styles.title} href="/">Portfolio</a>
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
-          src={menuOpen ? close : menu }
+          src={menuOpen ? close : menu}
           alt="menu-button"
           onClick={() => setMenuOpen(!menuOpen)}
         />
